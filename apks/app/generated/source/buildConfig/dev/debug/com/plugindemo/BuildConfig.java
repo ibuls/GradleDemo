2ebfaf6139185b2c2d0208279f7df2185433a2b0
Piyush <piyush.ibuls@gmail.com>
2020-03-15 01:55:07 +0530

    Initial commit

---
+++ apks/app/generated/source/buildConfig/dev/debug/com/plugindemo/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.plugindemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.plugindemo.dev";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "dev";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from product flavor: dev
+  public static final String testString = "Dev Mode";
+}
---
+++ apks/app/generated/source/buildConfig/dev/release/com/plugindemo/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.plugindemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.plugindemo.dev";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "dev";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from product flavor: dev
+  public static final String testString = "Dev Mode";
+}
